ba370d2d7bc6064d7dcc61a37727bb08505a8cfa
maquino96 <matthewaquino96@gmail.com>
2021-01-15 10:38:05 -0500

    setup

--- 05-many-to-many/starter_kit/models/doctor.rb
+++ 05-many-to-many/starter_kit/models/doctor.rb
@@ -1,3 +1,19 @@
 class Doctor
+   
+    @@all = []
+
+
+    attr_reader :name, :specialty
+
+    def initialize (name, specialty)
+        @name = name
+        @specialty = specialty
+        self.class.all << self 
+        @current_patients = []
+    end 
     
+    def self.all 
+        @@all
+    end 
+
 end
--- 05-many-to-many/starter_kit/models/patient.rb
+++ 05-many-to-many/starter_kit/models/patient.rb
@@ -1,3 +1,20 @@
 class Patient
     
-end
+    @@all = []
+    
+    def initialize(name, birth_year, doctor)
+        @name = name
+        @birth_year = birth_year 
+        @doctor = doctor 
+        @@all << self
+        # self.class.all 
+    end
+    
+    attr_reader :name, :birth_year
+    
+    def self.all
+        @@all 
+    end 
+    
+    
+    end
